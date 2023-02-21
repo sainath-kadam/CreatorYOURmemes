c4afd11d55182223d5715db63588987d63f78b1a
sainath-kadam <mydearsainathkadam@gmail.com>
2023-02-22 01:24:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "./Header"
+import MemesCreator from "./MemesCreator"
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <MemesCreator />
+        </div>
+    )
+}
+
+export default App
